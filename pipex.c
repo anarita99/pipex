4315d600807769db530f27f46366af6ea2c1f716
anarita99 <anaritadores@gmail.com>
2025-07-31 14:43:02 +0100

    IDK WHAT IM DOING

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: anarita <anarita@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 14:59:37 by adores            #+#    #+#             */
-/*   Updated: 2025/07/25 16:12:06 by anarita          ###   ########.fr       */
+/*   Updated: 2025/07/31 12:34:20 by anarita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@
 	//	printf("Hello from the main process\n");
 	return (0);
 }*/
-
+/*
 int main(int ac, char **av)
 {
 	int id = fork();
@@ -60,4 +60,147 @@ int main(int ac, char **av)
 	{
 		printf("\n");
 	}
+}*/
+
+/*int	main(int ac, char **av)
+{
+	int fd[2];
+	//fd[0] - read
+	//fd[1] - write
+	if (pipe(fd) == -1)
+	{
+		printf("ERROR");
+		return(1);
+	}
+	int id = fork();
+	if (id == -1)
+	{
+			printf("Error");
+			return (4);
+	}
+	if (id == 0)
+	{
+		close(fd[0]);
+		int x;
+		printf("Input a number: ");
+		scanf("%d", &x);
+		if(write(fd[1], &x, sizeof(int)) == -1)
+		{
+			printf("Error");
+			return (2);
+		}
+		close(fd[1]);
+	}
+	else
+	{
+		close(fd[1]);
+		int y;
+		if (read(fd[0], &y, sizeof(int)) == -1)
+		{
+			printf("Error");
+			return (3);
+		}
+		y = y * 3;
+		close(fd[0]);
+		printf("Got from child process: %d\n", y);
+	}
+	return (0);
+}*/
+
+/*int main(int argc, char **argv)
+{
+	int fd[2];
+	if (pipe(fd) == -1)
+	{
+		return (1);
+	}
+	int pid1 = fork();
+	if(pid1 < 0)
+		return (2);
+	if (pid1 == 0)
+	{	//Child process 1(ping)
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("ping", "ping", "-c", "5", "google.com", NULL);
+	}
+	int pid2 = fork();
+	if(pid2 < 0)
+		return (3);
+	if (pid2 == 0)
+	{	//Child process 1(ping)
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("grep", "grep", "rtt", NULL);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+	return(0);
+}*/
+
+void	call_child1(char **av, char **envp, int *fd)
+{
+	int file1;
+	file1 = open(av[1], O_RDONLY);
+	if (file1 < 0)
+	{
+		close(fd[0]);
+		close(fd[1]);
+	}
+	dup2(file1, STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
+	close(fd[0]);
+	close(fd[1]);
+}
+
+void	call_child2(char **av, char **envp, int *fd)
+{
+	int	file2;
+	file2 = open(av[4], O_WRONLY);
+	if (file2 < 0)
+	{
+		close(fd[0]);
+		close(fd[1]);
+	}
+	dup2(fd[0], STDIN_FILENO);
+	dup2(file2, STDOUT_FILENO);
+	
+	close(fd[0]);
+	close(fd[1]);
+}
+
+int	main(int ac, char **av, char *envp[])
+{
+	int		fd[2];
+	pid_t	proc_id[2];
+
+	if (ac == 5)
+	{
+		if(pipe(fd) == -1)
+			return(1);
+		proc_id[0] = fork();
+		if(proc_id[0] < 0)
+			return(1);
+		if(proc_id[0] == 0)
+			call_child1(av, envp, fd);
+		else
+		{
+			proc_id[1] = fork();
+			if (proc_id[1] < 0)
+				return(1);
+			if (proc_id[1] == 0)
+				call_child2(av, envp, fd);
+		}
+		close(fd[0]);
+		close(fd[1]);
+		waitpid(proc_id[0], NULL, 0);
+		waitpid(proc_id[1], NULL, 0);
+		return(0);
+	}
+	else
+		write(2, "Error", 5);
+		return(1);
 }
